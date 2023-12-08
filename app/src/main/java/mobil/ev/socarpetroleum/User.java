0e83e75f8c7cb7259b612466567bb67e6021aeb3
Mobil <qaraxanov@gmail.com>
2023-12-08 17:54:12 +0400

    vneseni izmeneniya

--- app/src/main/java/mobil/ev/socarpetroleum/User.java
+++ app/src/main/java/mobil/ev/socarpetroleum/User.java
@@ -1,27 +1,57 @@
 package mobil.ev.socarpetroleum;
 
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class User extends AppCompatActivity {
-   String polz_vez1;
+
+
+    private StorageReference ProductImageRef ;
+    private String downloadImageUrl;
+    private static final int GALLERYPICK = 1;
+    String polz_vez1;
     PersistantStorage  persistantStorage=new PersistantStorage();
     TextView polzAd,polzVez,polzYdm,polzAd1,polzId,polzSifre,polzTel;
+    ImageView UserImage;
     WebView webView;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     boolean[] x = {false};
+    private Uri ImageUri;
+    private ProgressDialog loadingBar;
+    private String saveCurrentDate,saveCurrentTime;
+    private String productRandomKey;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +66,7 @@ public class User extends AppCompatActivity {
         polzId = findViewById(R.id.et_id);
         polzAd1 = findViewById(R.id.textView6);
         polzSifre = findViewById(R.id.et_sifre);
+        UserImage= findViewById(R.id.imageButton);
 
       /*  this.webView=(WebView)findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
@@ -57,6 +88,32 @@ public class User extends AppCompatActivity {
         polzId.setText(UserInfo.getUser_id());
         polzSifre.setText(UserInfo.getUser_sifre());
         polzTel.setText(UserInfo.getUser_tel());
+        ProductImageRef = FirebaseStorage.getInstance().getReference().child("Product Images");
+
+        loadingBar = new ProgressDialog(this);
+        UserImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenGallery();
+            }
+        });
+    }
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,GALLERYPICK);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERYPICK && resultCode == RESULT_OK && data != null){
+            ImageUri = data.getData();
+            UserImage.setImageURI(ImageUri);
+        }
     }
 public void accountOut(View view){
     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
@@ -65,7 +122,8 @@ public void accountOut(View view){
     startActivity(intent);}
 
     public void accountSave(View view) {
-        DatabaseReference myRefX=database.getReference("in").child(UserInfo.getUser_ydm());
+        StoreProductInformation();
+       DatabaseReference myRefX=database.getReference("in").child(UserInfo.getUser_ydm());
         Map<String,String> map1=new HashMap<>();
         map1.put("ad",polzAd.getText().toString());
         map1.put("id",UserInfo.getUser_id());
@@ -79,8 +137,74 @@ public void accountOut(View view){
         UserInfo.setUser_sifre(polzSifre.getText().toString());
         persistantStorage.addProperty("login",UserInfo.getUser_ydm());
         persistantStorage.addProperty("sifre",UserInfo.getUser_sifre());
-    }}
+    }
+    
+    
+    private void StoreProductInformation() {
 
+      
+      loadingBar.setTitle("Загрузка данных");
+        loadingBar.setMessage("Пожалуйста, подождите...");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+       Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("ddMMyyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HHmmss");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        productRandomKey = saveCurrentDate + saveCurrentTime;
+
+        final StorageReference filePath = ProductImageRef.child(ImageUri.getLastPathSegment() + productRandomKey + ".jpg");
+
+     final UploadTask uploadTask = filePath.putFile(ImageUri);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                String message = e.toString();
+                Toast.makeText(User.this, "Ошибка: " + message, Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(User.this, "Изображение успешно загружено.", Toast.LENGTH_SHORT).show();
+
+                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if(!task.isSuccessful()){
+                            throw task.getException();
+                        }
+                        downloadImageUrl = filePath.getDownloadUrl().toString();
+                        return filePath.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if(task.isSuccessful()){
+                            downloadImageUrl = task.getResult().toString();
+
+                            Toast.makeText(User.this, "Фото сохранено", Toast.LENGTH_SHORT).show();
+
+                            // SaveProductInfoToDatabase();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+
+
+}
+    
   /*  @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
