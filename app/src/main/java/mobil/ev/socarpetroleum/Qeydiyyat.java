8664d727120767f118547ec6b78088d0f58b3465
Mobil <qaraxanov@gmail.com>
2023-12-10 22:11:24 +0400

    vneseni izmeneniya

--- app/src/main/java/mobil/ev/socarpetroleum/Qeydiyyat.java
+++ app/src/main/java/mobil/ev/socarpetroleum/Qeydiyyat.java
@@ -1,14 +1,20 @@
 package mobil.ev.socarpetroleum;
 
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,26 +24,60 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Qeydiyyat extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
     private EditText login,password;
     FirebaseAuth mAuth;
+    ImageView image_icon;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("in");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qeydiyyat);
         init();
+        image_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenGallery();
+            }
+        });
     }
+
+    private void OpenGallery() {
+
+      Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI) ;
+      startActivityForResult(intent,PICK_IMAGE_REQUEST);
+
+
+    }
+    @@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            final Uri resultUri = UCrop.getOutput(data);
+            if (resultUri != null) {
+                Picasso.get().load(resultUri).transform(new CircleTransform()).into(profileImageView);
+            }
+        }
+    }
+
+
+
     public void init(){
+
        login = (EditText)findViewById(R.id.et_login0) ;
         password = (EditText)findViewById(R.id.et_sifre0) ;
         mAuth= FirebaseAuth.getInstance();
+        image_icon = findViewById(R.id.image_edit);
     }
     @Override
     public void onStart() {
