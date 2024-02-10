d1f3335ad275e7e28bd0e025aaef2dc0f321be44
Mobil <qaraxanov@gmail.com>
2024-02-10 13:05:25 +0400

    bosyer

--- app/src/main/java/mobil/ev/socarpetroleum/OlcuFragment.java
+++ app/src/main/java/mobil/ev/socarpetroleum/OlcuFragment.java
@@ -69,7 +69,7 @@ public class OlcuFragment extends Fragment {
             public void onClick(View view) {
                 AlertDialog.Builder builder =new  AlertDialog.Builder( getContext());
                 builder.setTitle("Cənlərdə qalan boş yer")
-                        .setMessage("Ai92-"+(150000-bos_yer_s[0])+"\n"+"Dizel-"+(100000-bos_yer_s[1])+"\n"+"Premium-"+(25000-bos_yer_s[2]))
+                        .setMessage("Ai92-"+(150000-bos_yer_s[0])+"\n"+"Dizel-"+(100000-bos_yer_s[1])+"\n"+"Premium-"+(25000-bos_yer_s[2])+"\n"+"Super-"+(15000-bos_yer_s[3])+"\n"+"LPG-"+(30000-bos_yer_s[4]))
                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int i) {
